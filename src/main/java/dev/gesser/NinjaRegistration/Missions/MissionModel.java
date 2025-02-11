56c91ebd071af6193df4089b78ce247f2108be69
GaelGesser <gabrielhenrig@outlook.com>
2025-02-11 11:27:11 -0300

    feat: Place the Lombok in class for abstration

--- src/main/java/dev/gesser/NinjaRegistration/Missions/MissionModel.java
+++ src/main/java/dev/gesser/NinjaRegistration/Missions/MissionModel.java
@@ -2,11 +2,17 @@ package dev.gesser.NinjaRegistration.Missions;
 
 import dev.gesser.NinjaRegistration.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissionModel {
 
     @Id
